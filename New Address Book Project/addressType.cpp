65e37c44e426d96ed4048c215006eefd6857aaba
sb46539 <sb46539@email.vccs.edu>
2024-10-01 20:16:37 -0400

    asd

--- New Address Book Project/addressType.cpp
+++ New Address Book Project/addressType.cpp
@@ -1,1 +1,40 @@
 #include "addressType.h"
+#include <iostream>
+
+// Define constructor
+addressType::addressType(std::string addr, std::string cty, std::string st, int zip)
+    : address(addr), city(cty), state(st), zipcode(zip) {}
+
+
+void addressType::setAddress(std::string addr) {
+    address = addr;
+}
+
+void addressType::setCity(std::string cty) {
+    city = cty;
+}
+
+void addressType::setState(std::string st) {
+    if (st.length() == 2) {
+        state = st;
+    }
+    else
+    {
+        state = "XX";
+    }
+}
+
+void addressType::setZipcode(int zip) {
+    if (zip >= 11111 && zip <= 99999) {
+        zipcode = zip;
+    }
+    else
+    {
+        zipcode = 10000;
+    }
+}
+
+void addressType::print() {
+    std::cout << "Address: " << address << ", City: " << city
+        << ", State: " << state << ", Zipcode: " << zipcode << std::endl;
+}
--- New Address Book Project/addressType.h
+++ New Address Book Project/addressType.h
@@ -1,5 +1,30 @@
 #pragma once
+#include <string>
+#include <iostream>
+
+
+
 class addressType
 {
-};
+private:
+	std::string address;
+	std::string city;
+	std::string state;
+	int zipcode;
 
+public:
+	addressType(std::string address = "", std::string city = "", std::string state = "xx", int zipcode = 10000); // default constructor
+	// SETTERS
+	void setAddress(std::string address);
+	void setCity(std::string city);
+	void setState(std::string state);
+	void setZipcode(int zipcode);
+
+	// GETTERS	q
+	std::string getAddress();
+	std::string getCity();
+	std::string getState();
+	int getZipcode();
+
+	void print();
+};
