400c225da12c80d140800b6cc39a522653b7aa67
sb46539 <sb46539@email.vccs.edu>
2024-10-01 20:18:23 -0400

    asd

--- New Address Book Project/dateType.cpp
+++ New Address Book Project/dateType.cpp
@@ -1,1 +1,14 @@
 #include "dateType.h"
+#include <iostream>
+
+
+void dateType::setDate(int month, int day, int year) {
+    dMonth = month;
+    dDay = day;
+    dYear = year;
+}
+
+
+void dateType::print() const {
+    std::cout << dMonth << "/" << dDay << "/" << dYear << std::endl;
+}
--- New Address Book Project/dateType.h
+++ New Address Book Project/dateType.h
@@ -1,7 +1,28 @@
 #pragma once
 #include "addressType.h"
-class dateType :
-    public addressType
+class dateType : public addressType
 {
+private:
+    int dMonth;
+    int dDay;
+    int dYear;
+
+public:
+    dateType(int month = 1, int day = 1, int year = 1900) {
+        setDate(month, day, year);
+    }
+
+    void setDate(int month, int day, int year);
+
+    // GETTERS
+    int getDay() const;
+    int getMonth() const;
+    int getYear() const;
+
+
+    int getDaysInMonth(int month, int year) const;
+    bool isLeapYear(int year) const;
+
+    void print() const;
 };
 
